6af97963d23a16804fd103d574853b4e349a55a2
unknown <protik26@gmail.com>
2024-04-14 18:36:48 +1000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,109 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:mobile_draft/screen/camera_screen.dart';
+
+
+late List<CameraDescription> cameras;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: CameraApp(),
+    );
+  }
+}
+
+class CameraApp extends StatefulWidget {
+  const CameraApp({super.key});
+
+  @override
+  State<CameraApp> createState() => _CameraAppState();
+}
+
+class _CameraAppState extends State<CameraApp> {
+  late CameraController _controller;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller = CameraController(cameras[0], ResolutionPreset.max);
+    _controller.initialize().then((_){
+      if(!mounted){
+        return;
+      }
+      setState(() {
+        
+      });
+    }).catchError((Object e) {
+      if(e is CameraException){
+        switch (e.code) {
+          case 'CameraAccessDenied':
+            print("Access Denied");
+            break;
+          default:
+            print(e.description);
+            break;
+        }
+      }
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(children: [
+        Container(
+          height: double.infinity,
+          child:CameraPreview(_controller)
+          ),
+          //button to take picture
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Center(
+                child: Container(
+                  margin: EdgeInsets.all(20.0),
+                  child: MaterialButton(
+                      onPressed: () async {
+                        if(!_controller.value.isInitialized){
+                          return null;
+                        }
+                        if(_controller.value.isTakingPicture){
+                          return null;
+                        }
+
+                        try {
+                          await _controller.setFlashMode(FlashMode.auto);
+                          XFile file = await _controller.takePicture();
+                          // another screen to preview image
+                          Navigator.push(
+                            context, 
+                            MaterialPageRoute(
+                              builder: (context) => ImagePreview(file)));
+                        } on CameraException catch (e){
+                          debugPrint("Error occured while taking picture: $e");
+                          return null;
+                        }
+                      },
+                      color: Colors.white,
+                      child: Text("Take a picture"),
+                    ),
+                ),
+              )
+            ],
+          )
+      ]),
+    );
+  }
+}
---
+++ lib/screen/camera_screen.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+class ImagePreview extends StatefulWidget {
+  ImagePreview(this.file,{super.key});
+  XFile file;
+
+  @override
+  State<ImagePreview> createState() => _ImagePreviewState();
+}
+
+class _ImagePreviewState extends State<ImagePreview> {
+  @override
+  Widget build(BuildContext context) {
+    File picture = File(widget.file.path);
+    return Scaffold(
+      appBar: AppBar(title: Text("Preview Image"),),
+
+      body: Center(
+          child: Image.file(picture),
+        ),
+    );
+  }
+}
