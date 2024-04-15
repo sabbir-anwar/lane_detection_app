ca7507b49341fdc33d02fcdffd18fcb6fecb75f8
unknown <protik26@gmail.com>
2024-04-15 17:29:23 +1000

    image file upload draft

--- lib/screen/camera_screen.dart
+++ lib/screen/camera_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:camera/camera.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class ImagePreview extends StatefulWidget {
@@ -22,5 +23,36 @@ class _ImagePreviewState extends State<ImagePreview> {
           child: Image.file(picture),
         ),
     );
+  }
+}
+
+class ImageUploadService{
+  Future<void> uploadImage(XFile? pickedFile) async {
+    try{
+      Dio dio = Dio();
+
+      if(pickedFile !=null){
+        FormData formData = FormData.fromMap({
+          'userFile': await MultipartFile.fromFile(pickedFile.path,filename:'image.jpg'),
+        });
+
+        // send the POST request with the FormData
+        Response response = await dio.post(
+          'api endpoint',
+          data: formData,
+        );
+
+        if(response.statusCode == 200){
+          print('Image uploaded successfully');
+          print(response.data);
+        } else {
+          print('Image upload failed. Error: ${response.statusCode}');
+        }
+      } else {
+
+      }
+    } catch(error) {
+      print('Error uploading image: $error');
+    }
   }
 }
